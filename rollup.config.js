2657510db392f26e36287d1226bc2878bb7ff214
modem <tyler@modem.io>
2023-10-13 13:02:47 -0700

    consolidate rollup configs, replace old build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,34 @@
 import { defineConfig } from 'rollup'
 import ts from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
 import terser from '@rollup/plugin-terser'
 
-export default defineConfig({
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/bundle.mjs',
-      format: 'esm',
+export default defineConfig([
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.mjs',
+        format: 'esm',
+      },
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+      },
+    ],
+    plugins: [
+      terser(),
+      ts(),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
     },
-    {
-      file: 'dist/bundle.min.mjs',
-      format: 'esm',
-      plugins: [terser()],
-    },
-  ],
-  plugins: [
-    ts({
-      tsconfig: './tsconfig.json',
-    }),
-  ],
-})
+    plugins: [
+      dts(),
+    ],
+  }
+])
