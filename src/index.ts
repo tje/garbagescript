4b712716f621acaf50f61de537bbab789a0de322
modem <tyler@modem.io>
2023-10-28 15:02:37 -0700

    export createScript method, holding cached ast

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,52 @@ export const validate = (script: string, subjectData?: SubjectData, options?: Om
   return _evaluate(script, subjectData, options)
 }
 
+export const createScript = (script: string, options: IInterpreterOptions = {}) => {
+  let _tree!: ReturnType<typeof generateAST>
+  let _script: string
+
+  const _compile = (script: string) => {
+    const [ tokens, errs ] = scanSource(script)
+    if (errs.length > 0 && !options?.ignoreErrors) {
+      throw new Error(`Errors (${errs.length}) occurred parsing source: ${errs[0].msg}`)
+    }
+    _tree = generateAST(tokens)
+    _script = script
+  }
+
+  const _execute = (subjectData?: SubjectData) => {
+    const { run } = createInterpreter({
+      ...options,
+      subjectData: {
+        ...options.subjectData,
+        ...subjectData,
+      },
+    })
+    return run(_tree)
+  }
+
+  const evaluate = (subjectData?: SubjectData) => {
+    return _execute(subjectData).output
+  }
+
+  const validate = (subjectData?: SubjectData) => {
+    return _execute(subjectData)
+  }
+
+  _compile(script)
+
+  return {
+    get script () {
+      return _script
+    },
+    set script (s) {
+      _compile(s)
+    },
+    evaluate,
+    validate,
+  }
+}
+
 export { scanSource } from './scanner.js'
 export { Token } from './tokens.js'
 export { generateAST, ParseError } from './parser.js'
