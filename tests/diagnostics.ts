0c977d2139d0ba24e1d213ec403e5077df567548
modem <tyler@modem.io>
2024-01-12 14:31:43 -0700

    add tests for diagnostic results in conditional branches, iterators

--- tests/diagnostics.ts
+++ tests/diagnostics.ts
@@ -1,4 +1,4 @@
-import { test } from 'uvu'
+import { test, suite } from 'uvu'
 import * as assert from 'uvu/assert'
 import { validate } from '../src'
 
@@ -27,5 +27,50 @@ test('compare dates', () => {
   assert.equal(ev('1 month > 1 day'), [])
   assert.equal(ev('2 years < 1 year'), [])
 })
+
+undef_vars: {
+  const run = (script: string) => {
+    return validate(script, {}, { ignoreErrors: true, analyze: true }).diagnostics
+  }
+  const t = suite('undefined variables')
+  t('if true', () => {
+    const res = run('if true { $one }')
+    assert.equal(res.length, 1)
+    assert.match(res[0].message, 'variable')
+    assert.match(res[0].message, '$one')
+  })
+  t('if false', () => {
+    const res = run('if false { $one }')
+    assert.equal(res.length, 1)
+    assert.match(res[0].message, 'variable')
+    assert.match(res[0].message, '$one')
+  })
+  t('if true else', () => {
+    const res = run('if true { 1 } else { $one }')
+    assert.equal(res.length, 1)
+    assert.match(res[0].message, 'variable')
+    assert.match(res[0].message, '$one')
+  })
+  t('each empty', () => {
+    const res = run('each [] { $one }')
+    assert.equal(res.length, 1)
+    assert.match(res[0].message, 'variable')
+    assert.match(res[0].message, '$one')
+  })
+  t('each skip', () => {
+    const res = run('each [1] { skip $one }')
+    assert.equal(res.length, 1)
+    assert.match(res[0].message, 'variable')
+    assert.match(res[0].message, '$one')
+  })
+  t('each non-iterable', () => {
+    const res = run('each 1 { $one }')
+    assert.equal(res.length, 2)
+    assert.match(res[0].message, 'iterable')
+    assert.match(res[1].message, 'variable')
+    assert.match(res[1].message, '$one')
+  })
+  t.run()
+}
 
 test.run()
